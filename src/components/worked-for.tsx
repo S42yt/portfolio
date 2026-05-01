bf04f9b8433472b4368bd33dc318757223c7a197
s42 <songoku42@outlook.de>
2026-05-01 16:24:33 +0200

    add nrc

--- src/components/worked-for.tsx
+++ src/components/worked-for.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Marquee from "react-fast-marquee";
 
 export default function WorkedFor() {
-  const pfpImages = ["giggand.png", "tuubaa.png", "norisk.png", "holzi.png", "mr.png"];
+  const pfpImages = ["giggand.png", "tuubaa.png", "norisk.png", "holzi.png", "ren.png", "nrc.png"];
 
   return (
     <section id="worked-for" className="py-20">
@@ -45,14 +45,15 @@ export default function WorkedFor() {
                 const isTuubaa = imageName === "tuubaa";
                 const isNorisk = imageName === "norisk";
                 const isHolzi = imageName === "holzi";
-                const isRen = imageName === "mr";
-
+                const isRen = imageName === "ren";
+                const isNrc = imageName === "nrc";
                 const getLink = () => {
                   if (isGiggand) return "https://twitch.tv/giggand";
                   if (isTuubaa)  return "https://youtube.com/@tuubaa";
                   if (isNorisk)  return "https://youtube.com/@NoRiskk";
                   if (isHolzi)   return "https://www.youtube.com/@einHolzkopf";
                   if (isRen)     return "https://github.com/tsundosika";
+                  if (isNrc)     return "https://norisk.gg";
                   return null;
                 };
 
